be370bf84225a5a9f95c3c97d4fd4083570313e4
vaporcrash <sharvanram@gmail.com>
2023-06-01 22:14:47 -0700

    Skills adjusted

--- src/components/SectionTwo.jsx
+++ src/components/SectionTwo.jsx
@@ -4,7 +4,7 @@ import { SocialIcon } from 'react-social-icons';
 
 function SectionTwo(){
     return <ScrollPage className='Mid-Section Black'>
-        <Animator animation={batch(MoveIn(-1500,0),Fade(0,1),Sticky(25,37))}>
+        <Animator animation={batch(MoveIn(-1500,0),Fade(0,1),Sticky(25,53))}>
             <p className='Project-title'>Web3 Health Insurance</p>
             <div className='Web3-text'> 
                 <p>Smart contracts to create, redeem and prioritize insurance contracts</p>
@@ -15,7 +15,7 @@ Ethereum Blockchain</p>
             </div>
             <p><SocialIcon url='https://github.com/AvinashKartik/Health-Insurance-Using-Canisters' target='_blank' bgColor='white'/> <a className='jumps' href='https://github.com/AvinashKartik/Health-Insurance-Using-Canisters' target='_blank' rel="noreferrer">- Link to repository</a></p>
         </Animator>
-        <Animator animation={batch(MoveIn(1500,0),Fade(0,1),Sticky(70,50))} >
+        <Animator animation={batch(MoveIn(1500,0),Fade(0,1),Sticky(70,30))} >
             <p className='Project-title'>Yoyo Hotels</p>
             <div className='Web3-text'>
                 <p>Highly responsive web application for hotel owners and tourists to post and peruse a dynamic directory of
