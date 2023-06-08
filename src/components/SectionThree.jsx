c7709ec69fc0c14d0b5b7b31e0a2d81391eeac25
vaporcrash <sharvanram@gmail.com>
2023-06-07 19:28:42 -0700

    Realignment

--- src/components/SectionThree.jsx
+++ src/components/SectionThree.jsx
@@ -8,32 +8,32 @@ function SectionThree(){
         <Animator animation={batch(MoveIn(0,-100),Fade(0,1),Sticky(50,10))}>
             <p id='SkillPageTitle'>Skills</p>
         </Animator>
-        <Animator animation={batch(MoveIn(-700,0),Fade(0,1),Sticky(35,20))}>
+        <Animator animation={batch(MoveIn(-700,0),Fade(0,1),Sticky(30,20))}>
             <div className='Web3-text'>
             <p className='Skills-title'>Certifications</p>
             <p>AWS Certified Solutions Architect - Associate</p>
             <p>Google Cloud Certified Associate Cloud Engineer</p>
             </div>
         </Animator>
-        <Animator animation={batch(MoveIn(700,0),Fade(0,1),Sticky(65,35))}>
+        <Animator animation={batch(MoveIn(700,0),Fade(0,1),Sticky(70,35))}>
             <div className='Web3-text'>
             <p className='Skills-title'>Languages</p>
             <p className='Skills'> Python | Dart | C# | C | Java | HTML | CSS | Javascript | C++ | Motoko | Solidity | PL/SQL</p>
             </div>
         </Animator>
-        <Animator animation={batch(MoveIn(-700,0),Fade(0,1),Sticky(35,50))}>
+        <Animator animation={batch(MoveIn(-700,0),Fade(0,1),Sticky(30,50))}>
             <div className='Web3-text'>
             <p className='Skills-title'>Tools</p>
             <p className='Skills'>Unity | Android Studio | Git | PyCharm | Mongo Compass | Atlassian Suite</p>
             </div>
         </Animator>
-        <Animator animation={batch(MoveIn(700,0),Fade(0,1),Sticky(65,65))}>
+        <Animator animation={batch(MoveIn(700,0),Fade(0,1),Sticky(70,65))}>
             <div className='Web3-text'>
             <p className='Skills-title'>Frameworks</p>
             <p className='Skills'>Vue.js | Node.js | AWS | React.js | React-Native | Flutter | Express.js | Bootstrap | Flask | GCP</p>
             </div>
         </Animator>
-        <Animator animation={batch(MoveIn(-700,0),Fade(0,1),Sticky(35,80))}>
+        <Animator animation={batch(MoveIn(-700,0),Fade(0,1),Sticky(30,80))}>
             <div className='Web3-text'>
             <p className='Skills-title'>Libraries</p>
             <p className='Skills'>PyTorch | Pandas | Seaborn | DGL</p>
--- src/components/SectionTwo.jsx
+++ src/components/SectionTwo.jsx
@@ -5,23 +5,34 @@ import { SocialIcon } from 'react-social-icons';
 function SectionTwo(){
     return <ScrollPage className='Mid-Section Black'>
         <Animator animation={batch(MoveIn(-1500,0),Fade(0,1),Sticky(25,53))}>
-            <p className='Project-title'>Web3 Health Insurance</p>
+           
             <div className='Web3-text'> 
-                <p>Smart contracts to create, redeem and prioritize insurance contracts</p>
-                <p>Achieved average query time of 1 second on Internet Computer Blockchain, nearly 90% improvement from
+                <p className='Project-title'>Web3 Health Insurance</p>
+                <p>- Smart contracts to create, redeem and prioritize insurance contracts</p>
+                <p>- Achieved average query time of 1 second on Internet Computer Blockchain, nearly 90% improvement from
 Ethereum Blockchain</p>
-                <p>Coded on both Internet Computer and Ethereum Blockchains</p>
+                <p>- Coded on both Internet Computer and Ethereum Blockchains</p>
                 
-            </div>
+           
             <p><SocialIcon url='https://github.com/AvinashKartik/Health-Insurance-Using-Canisters' target='_blank' bgColor='white'/> <a className='jumps' href='https://github.com/AvinashKartik/Health-Insurance-Using-Canisters' target='_blank' rel="noreferrer">- Link to repository</a></p>
+            </div>
         </Animator>
-        <Animator animation={batch(MoveIn(1500,0),Fade(0,1),Sticky(70,30))} >
-            <p className='Project-title'>Yoyo Hotels</p>
+        <Animator animation={batch(MoveIn(1500,0),Fade(0,1),Sticky(70,20))} >
+            
             <div className='Web3-text'>
-                <p>Highly responsive web application for hotel owners and tourists to post and peruse a dynamic directory of
+                <p className='Project-title'>Yoyo Hotels</p>
+                <p>- Highly responsive web application for hotel owners and tourists to post and peruse a dynamic directory of
 hotels</p>
-                <p>Coded using Node.js, Mongoose and Bootstrap</p>
+                <p>- Coded using Node.js, Mongoose and Bootstrap</p>
                 <p><SocialIcon url='https://github.com/vaporcrash/Yoyo' target='_blank' bgColor='white'/> <a className='jumps' href='https://github.com/vaporcrash/Yoyo' target='_blank' rel="noreferrer">- Link to repository</a></p>
+            </div>
+        </Animator>
+        <Animator animation={batch(MoveIn(1500,0),Fade(0,1),Sticky(70,70))}>
+            <div className='Web3-text'>
+                <p className='Project-title'>Faux-La</p>
+                <p>- Simplified version of Ola, popular ridesharing application, coded in Java, implemented with Inheritance and Polymorphism</p>
+                <p>- Complete with 3 databases for drivers, customers and managers</p>
+                <p><SocialIcon url='https://github.com/vaporcrash/Faux-La' target='_blank' bgColor='white'/> <a className='jumps' href='https://github.com/vaporcrash/Faux-La' target='_blank' rel="noreferrer">- Link to repository</a></p>
             </div>
         </Animator>
     </ScrollPage>
